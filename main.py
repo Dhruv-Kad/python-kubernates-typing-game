4e10cfa14e78181fe8bef4ef4f63fbdce9136e4b
kadamdhruv98@gmail.com <bCwJKfsN9JRYaNC05DRdEjWkOA>
2021-07-26 13:08:44 -0500

    Added pycharm files to github

---
+++ main.py
@@ -0,0 +1,42 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+#sets starting values of words and "guesses"
+word = "apply"
+guess = ""
+#sets a while loop so that as long as the question remains incorrect, it will keep asking
+while guess != word:
+    guess = input(word +":")
+
+#sets next word
+word = "get"
+#Same loop as before
+while guess != word:
+    guess = input(word +":")
+#sets next word
+word = "get pods"
+#Same loop as before
+while guess != word:
+    guess = input(word +":")
+#sets next word
+word = "get nodes"
+#Same loop as before
+while guess != word:
+    guess = input(word +":")
+#sets next word
+word = "apply"
+#Same loop as before
+while guess != word:
+    guess = input(word +":")
+#sets next word
+word = "get"
+#Same loop as before
+while guess != word:
+    guess = input(word +":")
+
+
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
